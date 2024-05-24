d40b59b2cf2ad74df8edc23ad457b7ad2680e525
m.gavrilenko <m.gavrilenko@ngrsoftlab.ru>
2024-05-24 11:21:12 +0300

    fix: tests&cosmetics; feat: go upgrade -> 1.22.3 feat: experimental cache item onDeleteFunc support

--- README.md
+++ README.md
@@ -1,6 +1,66 @@
 # ngr-cacher
-Lib with little in-memory cacher
+Lib with little in-memory cacher (key - string, value - interface{}).
+In last version onDelete item function can be specified (experimental) 
 
+# import
 ```
 import "github.com/NGRsoftlab/ngr-cacher"
+```
+
+# plain example
+```
+// create cache instance with default expiration time = 5min, GC iteration timeout = 10min
+locCache := New(5*time.Minute, 10*time.Minute)
+
+// set string value into "testKey" element, set element expiration time = 60min
+// if you prefer using default expiration - just set 0 into this param
+locCache.Set("testKey", "testValue", 60*time.Minute)
+
+// get
+val, ok := locCache.Get("testKey")
+	if !ok {
+		fmt.Println("no such key")
+	}
+	
+// delete (no such key in cache)
+err := locCache.Delete("a?")
+	if err != nil {
+		fmt.Printf("can't delete: %s", err.Error())
+	}
+```
+
+# with onCloseFunc example (experimental feature)
+```
+// create cache instance with default expiration time = 5min, GC iteration timeout = 10min
+locCache := New(5*time.Minute, 10*time.Minute)
+onCloseFunc := func(item interface{}) error {
+						itemCast, ok := reflect.ValueOf(item).Interface().(chan int)
+						if !ok {
+							return errors.New("bad type cast")
+						}
+						close(itemCast)
+
+						return nil
+					}
+					
+// set chan int value into "testChanKey" element, set element expiration time = 60min, set onCloseFunc
+// if you prefer using default expiration - just set 0 into this param
+locCache.Set("testChanKey", make(chan int), 60*time.Minute, ItemOptions{onDeleteFunc: onCloseFunc})
+
+// get
+val, ok := locCache.Get("testChanKey")
+	if !ok {
+		fmt.Println("no such key")
+	}
+
+// delete (chan close func will be called inside locCache.Delete)
+err := locCache.Delete("a")
+	if err != nil {
+		fmt.Printf("can't delete: %s", err.Error())
+	}
+```
+
+# tests
+```
+just run go test
 ```
--- cacher.go
+++ cacher.go
@@ -1,14 +1,15 @@
-// Copyright 2020 NGR Softlab
-//
-// Cacher - little local in-memory cache.
-//
+// Copyright 2020-2024 (c) NGR Softlab
+
 package casher
 
 import (
 	"errors"
+	"fmt"
 	"sync"
 	"time"
 )
+
+////////////////////////////////////////// Struct and constructor
 
 // Cache struct cache
 type Cache struct {
@@ -23,11 +24,17 @@ type Item struct {
 	Value      interface{}
 	Expiration int64
 	Created    time.Time
+	Options    ItemOptions // experimental
 }
 
-// New. Initializing a new memory cache
-func New(defaultExpiration, cleanupInterval time.Duration) *Cache {
+// ItemOptions - item type info and on-delete func
+type ItemOptions struct {
+	setSign      bool
+	onDeleteFunc func(item interface{}) error
+}
 
+// New Initializing a new memory cache
+func New(defaultExpiration, cleanupInterval time.Duration) *Cache {
 	items := make(map[string]Item)
 
 	// cache item
@@ -44,9 +51,28 @@ func New(defaultExpiration, cleanupInterval time.Duration) *Cache {
 	return &cache
 }
 
-// Set setting a cache by key
-func (c *Cache) Set(key string, value interface{}, duration time.Duration) {
+// onDeleteProcessItem - on delete action wrapper (experimental)
+func (p *Item) processItemOnDeleteFunc() {
+	defer panicRecover()
+	if p.Options.setSign {
+		err := p.Options.onDeleteFunc(p.Value)
+		if err != nil {
+			fmt.Println("item onDeleteFunc error: ", err.Error())
+		}
+	}
+}
 
+// panicRecover - recover func for onDelete operations (for example)
+func panicRecover() {
+	if r := recover(); r != nil {
+		fmt.Println("Recovered in function: ", r)
+	}
+}
+
+////////////////////////////////////////// Get-Set
+
+// Set setting a cache by key
+func (c *Cache) Set(key string, value interface{}, duration time.Duration, options ...ItemOptions) {
 	var expiration int64
 
 	if duration == 0 {
@@ -58,22 +84,27 @@ func (c *Cache) Set(key string, value interface{}, duration time.Duration) {
 	}
 
 	c.Lock()
-
 	defer c.Unlock()
+
+	// set into item options only 1st options object
+	itemOptions := ItemOptions{}
+	if len(options) > 0 {
+		itemOptions.setSign = true
+		itemOptions.onDeleteFunc = options[0].onDeleteFunc
+	}
 
 	c.items[key] = Item{
 		Value:      value,
 		Expiration: expiration,
 		Created:    time.Now(),
+		Options:    itemOptions,
 	}
 
 }
 
 // Get getting a cache by key
 func (c *Cache) Get(key string) (interface{}, bool) {
-
 	c.RLock()
-
 	defer c.RUnlock()
 
 	item, found := c.items[key]
@@ -84,7 +115,6 @@ func (c *Cache) Get(key string) (interface{}, bool) {
 	}
 
 	if item.Expiration > 0 {
-
 		// cache expired
 		if time.Now().UnixNano() > item.Expiration {
 			return nil, false
@@ -95,65 +125,35 @@ func (c *Cache) Get(key string) (interface{}, bool) {
 	return item.Value, true
 }
 
-// Delete cache by key
-// Return false if key not found
-func (c *Cache) Delete(key string) error {
+////////////////////////////////////////// Keys-Items
 
-	c.Lock()
-
-	defer c.Unlock()
-
-	if _, found := c.items[key]; !found {
-		return errors.New("Key not found")
-	}
-
-	delete(c.items, key)
-
-	return nil
-}
-
-// StartGC start Garbage Collection
-func (c *Cache) StartGC() {
-	go c.GC()
-}
-
-// GC Garbage Collection
-func (c *Cache) GC() {
-
-	for {
-
-		<-time.After(c.cleanupInterval)
-
-		if c.items == nil {
-			return
-		}
-
-		if keys := c.ExpiredKeys(); len(keys) != 0 {
-			c.clearItems(keys)
-
-		}
-
-	}
-
-}
-
-// expiredKeys returns key list which are expired.
+// GetItems returns items list
 func (c *Cache) GetItems() (items []string) {
-
 	c.RLock()
-
 	defer c.RUnlock()
 
-	for k, _ := range c.items {
+	for k := range c.items {
 		items = append(items, k)
 	}
 
 	return
 }
 
-// expiredKeys returns key list which are expired.
-func (c *Cache) ExpiredKeys() (keys []string) {
+// GetKeys returns key list
+func (c *Cache) GetKeys() (keys []string) {
+	c.RLock()
 
+	defer c.RUnlock()
+
+	for k := range c.items {
+		keys = append(keys, k)
+	}
+
+	return
+}
+
+// ExpiredKeys returns key list which are expired
+func (c *Cache) ExpiredKeys() (keys []string) {
 	c.RLock()
 
 	defer c.RUnlock()
@@ -169,12 +169,71 @@ func (c *Cache) ExpiredKeys() (keys []string) {
 
 // clearItems removes all the items which key in keys.
 func (c *Cache) clearItems(keys []string) {
-
 	c.Lock()
 
 	defer c.Unlock()
 
 	for _, k := range keys {
+		item, ok := c.items[k]
+		if ok {
+			item.processItemOnDeleteFunc()
+		}
+
+		delete(c.items, k)
+	}
+}
+
+////////////////////////////////////////// Cleaning
+
+// StartGC start Garbage Collection
+func (c *Cache) StartGC() {
+	go c.GC()
+}
+
+// GC Garbage Collection
+func (c *Cache) GC() {
+	for {
+		<-time.After(c.cleanupInterval)
+
+		if c.items == nil {
+			return
+		}
+
+		if keys := c.ExpiredKeys(); len(keys) != 0 {
+			c.clearItems(keys)
+		}
+
+	}
+}
+
+// Delete cache by key. Return false if key not found
+func (c *Cache) Delete(key string) error {
+	c.Lock()
+	defer c.Unlock()
+
+	item, ok := c.items[key]
+	if !ok {
+		return errors.New("key's not found")
+	}
+
+	item.processItemOnDeleteFunc()
+	delete(c.items, key)
+
+	return nil
+}
+
+// ClearAll - remove all items
+func (c *Cache) ClearAll() {
+	c.Lock()
+	defer c.Unlock()
+
+	for k := range c.items {
+		item, ok := c.items[k]
+
+		if ok {
+			item.processItemOnDeleteFunc()
+		}
+
 		delete(c.items, k)
 	}
 }
